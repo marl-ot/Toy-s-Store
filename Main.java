1175296e7a1e30f31d6a4081ea89c592d2051a90
marl-ot <marlot.swyat@gmail.com>
2023-09-08 21:09:12 +0300

    Update Main.java file

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ToyStore toyStore = new ToyStore();
-
-        Toy toy1 = new Toy("1", "Кукла", 5, toyStore);
-        Toy toy2 = new Toy("2", "Робот", 2, toyStore);
-        Toy toy3 = new Toy("3", "Пазл", 6, toyStore);
-
-        toyStore.addToy(toy1);
-        toyStore.addToy(toy2);
-        toyStore.addToy(toy3);
-
-        toyStore.simulateToySelection(10, "toy_results.txt");
+    ToyStore toyStore = new ToyStore();
+    
+    // Добавление игрушек
+    Toy toy1 = new Toy("1", "Кукла", 10, 20);
+    Toy toy2 = new Toy("2", "Мяч", 15, 20);
+    Toy toy3 = new Toy("3", "Пазл", 5, 60);
+    
+    toyStore.addToy(toy1);
+    toyStore.addToy(toy2);
+    toyStore.addToy(toy3);
+    
+    // Выбор призовых игрушек и запись их в файл
+    for (int i = 0; i < 10; i++) {
+        toyStore.choosePrizeToy();
+        List<Toy> prizeToys = toyStore.getPrizeToys();
+        if (i < prizeToys.size()) {
+            Toy prizeToy = prizeToys.get(i);
+            toyStore.getPrizeToyAndWriteToFile("prize_results.txt", prizeToy);
+        }
     }
+}
 }
