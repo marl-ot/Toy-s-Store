603c0f2bab305b232fdbf8d1108d27d506f48c27
marl-ot <marlot.swyat@gmail.com>
2023-09-08 21:09:38 +0300

    Update ToyStore.java file

--- ToyStore.java
+++ ToyStore.java
@@ -1,38 +1,80 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class ToyStore {
     private Random random = new Random();
     private PriorityQueue<Toy> toyQueue = new PriorityQueue<>((t1, t2) -> t2.getFrequency() - t1.getFrequency());
+    private List<Toy> prizeToys = new ArrayList<>();
 
     public void addToy(Toy toy) {
         toyQueue.add(toy);
     }
 
-    public Toy getRandomToy() {
-        double randomValue = random.nextDouble();
-        double cumulativeProbability = 0;
-
+    public void updateToyWeight(String toyId, int newWeight) {
         for (Toy toy : toyQueue) {
-            cumulativeProbability += toy.getProbability();
-            if (randomValue < cumulativeProbability) {
+            if (toy.getId().equals(toyId)) {
+                toy.setFrequency(newWeight);
+                toyQueue.remove(toy);
+                toyQueue.add(toy);
+                break;
+            }
+        }
+    }
+
+    public Toy getRandomToy() {
+        double totalWeight = toyQueue.stream().mapToDouble(Toy::getFrequency).sum();
+        double randomValue = random.nextDouble() * totalWeight;
+
+        double cumulativeWeight = 0;
+        for (Toy toy : toyQueue) {
+            cumulativeWeight += toy.getFrequency();
+            if (randomValue <= cumulativeWeight) {
                 return toy;
             }
         }
 
-        // Вернуть случайную игрушку, если ничего не выбрано
-        return getRandomToy();
+        return null;
+    }
+
+    public List<Toy> getPrizeToys() {
+        return prizeToys;
+    }
+
+    public void choosePrizeToy() {
+        Toy selectedToy = getRandomToy();
+        if (selectedToy != null) {
+            prizeToys.add(selectedToy);
+            decreaseToyQuantity(selectedToy);
+        }
+    }
+
+    public void getPrizeToyAndWriteToFile(String outputFileName, Toy prizeToy) {
+        if (prizeToy != null) {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName, true))) {
+                writer.write("Получена призовая игрушка: ID=" + prizeToy.getId() + ", Название=" + prizeToy.getName());
+                writer.newLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void simulateToySelection(int numberOfSelections, String outputFileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
             for (int i = 0; i < numberOfSelections; i++) {
                 Toy selectedToy = getRandomToy();
-                writer.write("Выбрана игрушка: ID=" + selectedToy.getId() + ", Название=" + selectedToy.getName() + ", Частота=" + selectedToy.getFrequency());
-                writer.newLine();
+                if (selectedToy != null) {
+                    writer.write("Выбрана игрушка: ID=" + selectedToy.getId() + ", Название=" + selectedToy.getName() + ", Частота=" + selectedToy.getFrequency());
+                    writer.newLine();
+                } else {
+                    writer.write("Игрушки закончились");
+                    writer.newLine();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -46,4 +88,14 @@ public class ToyStore {
         }
         return totalFrequency;
     }
+
+    public void decreaseToyQuantity(Toy toy) {
+        for (Toy t : toyQueue) {
+            if (t.getId().equals(toy.getId())) {
+                t.decrementQuantity();
+                break;
+            }
+        }
+    }
 }
+
